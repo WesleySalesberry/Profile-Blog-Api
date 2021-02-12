16608a8b886f3536a03876191fcdee8b79da4476
Wes <wesleySalesberry@hotmail.com>
2021-02-12 17:16:12 -0500

    added the project model for mongo db

--- models/Project.js
+++ models/Project.js
@@ -15,10 +15,11 @@ const ProjectSchema = new mongoose.Schema({
     },
     languages:{
         type:[String],
-        required: false, 
+        required: true, 
     },
     website: {
         type: String,
+        required: false,
         match: [
         /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
         'Please use a valid URL with HTTP or HTTPS'
@@ -26,11 +27,16 @@ const ProjectSchema = new mongoose.Schema({
     },
     github: {
         type: String,
+        required: false,
         match: [
         /https?:\/\/(www\.)?github\.[a-zA-Z0-9()]{1,6}\b/,
         'Please use a valid URL with HTTP or HTTPS'
       ]
-    }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
 })
 
-https?:\/\/github\.com\/
+module.exports = mongoose.model('Project', ProjectSchema)
