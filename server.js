da10fadd6df8b2054061700b792bdac2a9cf1c64
Wes <wesleySalesberry@hotmail.com>
2021-02-16 14:00:23 -0500

    added packages to display yaml file

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require('express')
 const app = express()
 
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const colors = require('colors')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const path = require('path')
+const swaggerUI = require("swagger-ui-express");
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml');
+//const swaggerJsDoc = require("swagger-jsdoc");
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 
-const connectDB = require('./config/db')
+const errorHandler = require('./middlewares/errorHandler');
 
-//app.use(express.static(path.join(__dirname, 'public')))
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 5000
+
+// app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 app.use(fileUpload())
 
 const blog = require('./routes/blog')
@@ -20,19 +30,13 @@ const comment = require('./routes/comment')
 const projects = require('./routes/project')
 const auth = require('./routes/auth')
 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/blog', blog)
 app.use('/api/v1/comment', comment)
 app.use('/api/v1/auth', auth)
 app.use('/api/v1/project', projects)
 
-//@test Logging routes to compare to API Call
-dotenv.config({ path: './config/config.env'})
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
-}
-
-connectDB()
-const PORT = process.env.PORT || 5000
+app.use(errorHandler)
 
 
 //If the route is not a valid route 
@@ -42,7 +46,16 @@ app.get('*', (req, res) => {
         message: "Invalid Route"
     })
 });
-const server = app.listen(PORT, console.log(`> Server running in ${process.env.NODE_ENV}: http://localhost:${PORT}`.brightBlue.bold))
+
+//@test Logging routes to compare to API Call
+dotenv.config({ path: './config/config.env'})
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+connectDB()
+
+const server = app.listen(PORT, console.log(`> Server running in ${process.env.NODE_ENV}: http://localhost:${PORT}/api-docs/`.brightBlue.bold))
 //Handle Rejection
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`.red)
