0398b066d3c155fa23b9c9750d73310b9fdb60b4
Wes <wesleySalesberry@hotmail.com>
2021-03-06 11:06:53 -0500

    added get user

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-const { login, register, logout } = require('../controllers/auth')
+const { login, register, logout, getUser } = require('../controllers/auth')
+const { protect } = require('../middlewares/protect')
 
 router
     .route('/login')
     .post(login)
+
+router
+    .route('/user')
+    .get(protect, getUser)
 
 router
     .route('/register')
